5b300bd5f05f5f805d5afffeadf5792c9445816d
Peak <pglund18@yahoo.com>
2023-09-26 19:05:24 -0500

    netlify works?

--- vite-project/src/pages/Resume.jsx
+++ vite-project/src/pages/Resume.jsx
@@ -5,9 +5,9 @@ export default function Resume() {
     <div className="container mt-5">
     <h1>Resume</h1>
     <p>
-      You can download my full resume{''}
+      You can download my full {''}
       <a href="/" target="_blank" rel="noopener noreferrer">
-        here
+        Resume
       </a>
       .
     </p>
